ce6620891ab9b7faf017620f2bfa9eac99fe9fb6
Tomy Maulana <stealthflanker93@gmail.com>
2026-02-20 16:18:26 +0700

    Update TwoFactorChallengeTest to use active user factory for two-factor authentication tests

--- tests/Feature/Auth/TwoFactorChallengeTest.php
+++ tests/Feature/Auth/TwoFactorChallengeTest.php
@@ -24,7 +24,7 @@ test('two factor challenge can be rendered', function () {
         'confirmPassword' => true,
     ]);
 
-    $user = User::factory()->create();
+    $user = User::factory()->active()->create();
 
     $user->forceFill([
         'two_factor_secret' => encrypt('test-secret'),
