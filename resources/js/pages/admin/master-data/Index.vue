db1a85d7dda81be3c78490e458f9426e89d55dde
Tomy Maulana <stealthflanker93@gmail.com>
2026-06-10 11:12:28 +0700

    Fix button layout alignment on master-data and settings index pages

--- resources/js/pages/admin/master-data/Index.vue
+++ resources/js/pages/admin/master-data/Index.vue
@@ -98,9 +98,13 @@ const masterDataCard = computed<CardItem[]>(() => {
         :description="trans('admin.master_data.description')"
     >
         <div class="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
-            <Card v-for="item in masterDataCard" :key="item.title">
-                <CardContent class="flex items-center gap-4">
-                    <component :is="item.icon" class="size-6" />
+            <Card
+                v-for="item in masterDataCard"
+                :key="item.title"
+                class="flex h-full flex-col"
+            >
+                <CardContent class="flex flex-1 items-start gap-4">
+                    <component :is="item.icon" class="mt-0.5 size-6" />
                     <div class="flex flex-col gap-1">
                         <h3 class="text-sm font-medium">{{ item.title }}</h3>
                         <p class="text-sm text-muted-foreground">
@@ -108,7 +112,7 @@ const masterDataCard = computed<CardItem[]>(() => {
                         </p>
                     </div>
                 </CardContent>
-                <CardFooter>
+                <CardFooter class="pt-0">
                     <Button :as="Link" :href="item.href">
                         {{ trans('admin.master_data.action.open') }}
                     </Button>
